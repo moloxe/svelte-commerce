f23822d53d89de68a12f7c5dd1d95f86141943b1
Franco Moloche <dev@moloxe.io>
2024-02-11 10:10:49 -0500

    Add content-type header to GET response in products server

--- src/routes/api/products/+server.ts
+++ src/routes/api/products/+server.ts
@@ -3,5 +3,9 @@ import { products } from '$lib/data/products';
 export const prerender = true;
 
 export async function GET() {
-	return new Response(JSON.stringify({ products }));
+	return new Response(JSON.stringify({ products }), {
+		headers: {
+			'content-type': 'application/json'
+		}
+	});
 }
